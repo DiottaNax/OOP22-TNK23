80aa0748e949d39a5213efe792bb530f52171f9f
Nax <federico.diotallevi@studio.unibo.it>
2023-04-26 15:12:39 +0200

    Moved GameEngine from it/unibo/tnk23/model/api to it/unibo/tnk23/core/api/

--- src/main/java/it/unibo/tnk23/core/api/GameEngine.java
+++ src/main/java/it/unibo/tnk23/core/api/GameEngine.java
@@ -1,4 +1,7 @@
-package it.unibo.tnk23.model.api;
+package it.unibo.tnk23.core.api;
+
+import it.unibo.tnk23.model.api.GameState;
+import it.unibo.tnk23.model.api.World;
 
 public interface GameEngine {
     
