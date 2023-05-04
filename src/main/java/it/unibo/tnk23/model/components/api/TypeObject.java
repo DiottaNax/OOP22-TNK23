e4f6d3524e68ce6c7f218b0323c9034b1a7c211f
Nax <federico.diotallevi@studio.unibo.it>
2023-05-04 16:29:12 +0200

    [DEV] Added TypeObject system pattern, with its factory and test

---
+++ src/main/java/it/unibo/tnk23/model/components/api/TypeObject.java
@@ -0,0 +1,13 @@
+package it.unibo.tnk23.model.components.api;
+
+public interface TypeObject {
+
+    long getWidth();
+
+    long getheight();
+
+    double getSpeed();
+
+    long getHealth();
+    
+}
