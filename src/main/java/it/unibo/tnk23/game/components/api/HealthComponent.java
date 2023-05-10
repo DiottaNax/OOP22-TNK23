13f5512a80b7c4b67b883fb6f763fe1a77dc0f92
Javid Ameri <javid.ameri@studio.unibo.it>
2023-05-10 22:28:56 +0200

    added class HealthComponent and updated GameObject

---
+++ src/main/java/it/unibo/tnk23/game/components/api/HealthComponent.java
@@ -0,0 +1,31 @@
+package it.unibo.tnk23.game.components.api;
+
+import it.unibo.tnk23.game.model.api.GameObject;
+import it.unibo.tnk23.game.world.api.World;
+
+public abstract class HealthComponent extends AbstractComponent implements NotifiableComponent {
+
+    private long health;
+
+    public HealthComponent(GameObject entity, World world) {
+        super(entity, world);
+        this.health = entity.getType().getHealth();
+    }
+
+    protected abstract void performWhenHit(GameObject entity);
+
+    @Override
+    public void update() {
+        if (health == 0) {
+            //world.notifyEvent();
+        }
+    }
+
+    @Override
+    public <X> void receive(Message<X> x) {
+        if (x instanceof GameObject) {
+            performWhenHit((GameObject)x);
+        }
+    }
+    
+}
