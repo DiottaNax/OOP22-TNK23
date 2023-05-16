8291a26cd1c656427cc4c808f4b9b9a92a5dbe28
Nax <federico.diotallevi@studio.unibo.it>
2023-05-16 22:34:56 +0200

    [FIX] swapped east and west

--- src/main/java/it/unibo/tnk23/common/Directions.java
+++ src/main/java/it/unibo/tnk23/common/Directions.java
@@ -3,8 +3,8 @@ package it.unibo.tnk23.common;
 public enum Directions {
     NORTH(0, -1),
     SOUTH(0, 1),
-    EAST(-1, 0),
-    WEST(1, 0),
+    WEST(-1, 0),
+    EAST(1, 0),
     NONE(0, 0);
 
     private Vector2D direction;
