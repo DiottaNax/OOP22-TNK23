a1f1cfcbb5ecda0622f253f6f6ff77967ee68590
Javid Ameri <javid.ameri@studio.unibo.it>
2023-05-18 18:47:48 +0200

    [DEV] created an Input component

---
+++ src/main/java/it/unibo/tnk23/game/components/impl/InputComponent.java
@@ -0,0 +1,23 @@
+package it.unibo.tnk23.game.components.impl;
+
+import it.unibo.tnk23.game.components.api.Component;
+import it.unibo.tnk23.game.input.api.InputController;
+import it.unibo.tnk23.game.model.api.GameObject;
+
+public class InputComponent implements Component {
+
+    GameObject player;
+    InputController ctrl;
+
+    public InputComponent(GameObject player, InputController ctrl) {
+        this.ctrl = ctrl;
+        this.player = player;
+    }
+
+    @Override
+    public void update() {
+        
+        player.getType().getSpeed()
+    }
+
+}
