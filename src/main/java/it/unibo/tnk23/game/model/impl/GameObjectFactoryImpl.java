99013658dcbdc577bd69cbdeb1aa10c1b4057699
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2023-05-25 16:53:56 +0200

    [DEV] Implemented GameObjectFactoryImpl

--- src/main/java/it/unibo/tnk23/game/model/impl/GameObjectFactoryImpl.java
+++ src/main/java/it/unibo/tnk23/game/model/impl/GameObjectFactoryImpl.java
@@ -2,10 +2,11 @@ package it.unibo.tnk23.game.model.impl;
 
 import it.unibo.tnk23.common.Point2D;
 import it.unibo.tnk23.game.components.impl.BulletComponent;
+import it.unibo.tnk23.game.components.impl.BulletHealthComponent;
 import it.unibo.tnk23.game.components.impl.CollisionComponent;
+import it.unibo.tnk23.game.components.impl.EntitiesHealthComponent;
 import it.unibo.tnk23.game.components.impl.PhysicsComponent;
 import it.unibo.tnk23.game.components.impl.PlayerFireComponent;
-import it.unibo.tnk23.game.components.impl.TankHealthComponent;
 import it.unibo.tnk23.game.components.impl.TimeFireComponent;
 import it.unibo.tnk23.game.model.api.GameObject;
 import it.unibo.tnk23.game.model.api.GameObjectFactory;
@@ -23,7 +24,7 @@ public class GameObjectFactoryImpl implements GameObjectFactory {
     @Override
     public GameObject getEnemy(Point2D pos) {
         var enemy = new GameObjectImpl(TypeObjectFactory.getEnemyType(), pos);
-        enemy.addComponent(new TankHealthComponent(enemy, world));
+        enemy.addComponent(new EntitiesHealthComponent(enemy, world));
         enemy.addComponent(new TimeFireComponent(enemy, world));
         enemy.addComponent(new CollisionComponent(enemy, world));
         return enemy;
@@ -32,7 +33,7 @@ public class GameObjectFactoryImpl implements GameObjectFactory {
     @Override
     public GameObject getPlayer(Point2D pos) {
         var player = new GameObjectImpl(TypeObjectFactory.getPlayerType(), pos);
-        player.addComponent(new TankHealthComponent(player, world));
+        player.addComponent(new EntitiesHealthComponent(player, world));
         player.addComponent(new PlayerFireComponent(player, world));
         player.addComponent(new CollisionComponent(player, world));
         return player;
@@ -41,7 +42,7 @@ public class GameObjectFactoryImpl implements GameObjectFactory {
     @Override
     public GameObject getBullet(Point2D pos) {
         var bullet = new GameObjectImpl(TypeObjectFactory.getBulletType(), pos);
-        bullet.addComponent(new HealthComponent(bullet, world));
+        bullet.addComponent(new BulletHealthComponent(bullet, world));
         bullet.addComponent(new CollisionComponent(bullet, world));
         bullet.addComponent(new BulletComponent(bullet, world));
         bullet.addComponent(new PhysicsComponent(bullet, world));
@@ -61,14 +62,14 @@ public class GameObjectFactoryImpl implements GameObjectFactory {
         var destroyableWall = new GameObjectImpl(TypeObjectFactory.getObstacleType(), pos);
         destroyableWall.addComponent(new PhysicsComponent(destroyableWall, world));
         destroyableWall.addComponent(new CollisionComponent(destroyableWall, world));
-        destroyableWall.addComponent(new HealthComponent(destroyableWall, world));
+        destroyableWall.addComponent(new EntitiesHealthComponent(destroyableWall, world));
         return destroyableWall;
     }
 
     @Override
     public GameObject getTower(Point2D pos) {
         var twr = new GameObjectImpl(TypeObjectFactory.getObstacleType(), pos);
-        twr.addComponent(new HealthComponent(twr, world));
+        twr.addComponent(new EntitiesHealthComponent(twr, world));
         twr.addComponent(new CollisionComponent(twr, world));
         twr.addComponent(new PhysicsComponent(twr, world));
         return twr;
