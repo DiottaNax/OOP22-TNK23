b8968ec426769c139dfd5529eeb09740c25ff2c6
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2023-05-25 19:07:45 +0200

    [DEV] Added impl folder and SceneFactoryImpl

---
+++ src/main/java/it/unibo/tnk23/view/impl/SceneFactoryImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.tnk23.view.impl;
+
+import it.unibo.tnk23.view.api.SceneFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class SceneFactoryImpl implements SceneFactory{
+
+    @Override
+    public Scene getMenuScene() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMenuScene'");
+    }
+
+    @Override
+    public Scene getGameScene(Parent root) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameScene'");
+    }
+
+    @Override
+    public Scene getGameOverScene() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameOverScene'");
+    }
+    
+}
