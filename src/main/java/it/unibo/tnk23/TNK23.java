095c00b8397139ce4fe3f902509ff1181a3026b6
Nax <federico.diotallevi@studio.unibo.it>
2023-05-26 13:32:31 +0200

    [DEV] added main class and application class

---
+++ src/main/java/it/unibo/tnk23/TNK23.java
@@ -0,0 +1,16 @@
+package it.unibo.tnk23;
+
+import javafx.application.Application;
+
+public final class TNK23 {
+
+    /**
+     * The constructor of a utility class should never be called
+     */
+    private TNK23() {
+    }
+
+    public static void main(String... args) {
+        Application.launch(TNK23App.class, args);
+    }
+}
