6459bfc1ff3d20727850b32ecae035710075e793
Nax <federico.diotallevi@studio.unibo.it>
2023-05-26 11:45:43 +0200

    [DEV] added a basic implementation of AbstractGameView

---
+++ src/main/java/it/unibo/tnk23/view/api/AbstractGameView.java
@@ -0,0 +1,39 @@
+package it.unibo.tnk23.view.api;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public abstract class AbstractGameView implements GameView {
+
+    protected final Stage stage;
+    protected final SceneFactory sceneFactory;
+    protected final RenderingEngine renderingEngine;
+    
+    public AbstractGameView(final Stage stage) {
+        this.sceneFactory = null; //to set when implemented
+        this.renderingEngine = null; //to set when implemented
+        this.stage = stage;     
+    }
+
+    @Override
+    public void renderView() {
+        Platform.runLater(renderingEngine);
+    }
+
+    @Override
+    public void setGameOverScene() {
+        this.stage.setScene(this.sceneFactory.getMenuScene());
+    }
+
+    @Override
+    public void setGameScene() {
+        this.stage.setScene(
+                this.sceneFactory.getGameScene(this.renderingEngine.getGameSceneRoot()));
+    }
+
+    @Override
+    public void setMenuScene() {
+        this.stage.setScene(this.sceneFactory.getMenuScene());
+    }
+    
+}
