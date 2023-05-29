1979df14414c2aadf3e8393b1cd66bdb8055cacb
Nax <federico.diotallevi@studio.unibo.it>
2023-05-29 20:42:21 +0200

    [DEV] the World is generated starting from a GameMap

--- src/main/java/it/unibo/tnk23/game/model/impl/WorldImpl.java
+++ src/main/java/it/unibo/tnk23/game/model/impl/WorldImpl.java
@@ -1,12 +1,15 @@
 package it.unibo.tnk23.game.model.impl;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import it.unibo.tnk23.game.components.impl.GraphicComponent;
 import it.unibo.tnk23.game.events.api.WorldEvent;
 import it.unibo.tnk23.game.events.api.WorldEventListener;
+import it.unibo.tnk23.game.model.api.GameMap;
 import it.unibo.tnk23.game.model.api.GameObject;
 import it.unibo.tnk23.game.model.api.World;
 
@@ -16,10 +19,23 @@ public class WorldImpl implements World {
     private Set<GameObject> entities;
     private Set<GameObject> obstacles;
     private GameObject tower;
+    private final GameMap gameMap;
     private WorldEventListener weListener;
 
-    public WorldImpl(final List<GameObject> players) {
+    public WorldImpl(final List<GameObject> players, final GameMap gameMap) {
         this.players = players;
+        this.gameMap = gameMap;
+        this.obstacles = new HashSet<>();
+        this.entities = new HashSet<>();
+
+        var objFactory = new GameObjectFactoryImpl(this);
+        var toAdd = gameMap.getWalls().stream().map(objFactory::getWall);
+        toAdd.forEach(w -> w.addComponent(new GraphicComponent(w, "wall")));
+        this.obstacles.addAll(toAdd.toList());
+
+        toAdd = gameMap.getDestroyableWalls().stream().map(objFactory::getDestroyableWall);
+        toAdd.forEach(w -> w.addComponent(new GraphicComponent(w, "destroyableWall")));
+        this.obstacles.addAll(toAdd.toList());
     }
 
     @Override
