99d366b92d72ec8380992aa5422f2ed06d9feb6e
Nax <federico.diotallevi@studio.unibo.it>
2023-05-29 16:16:52 +0200

    [DEV] added a method that takes an id and returns the corresponding player

--- src/main/java/it/unibo/tnk23/game/model/api/World.java
+++ src/main/java/it/unibo/tnk23/game/model/api/World.java
@@ -1,5 +1,7 @@
 package it.unibo.tnk23.game.model.api;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import it.unibo.tnk23.game.events.api.WorldEvent;
@@ -7,7 +9,9 @@ import it.unibo.tnk23.game.events.api.WorldEventListener;
 
 public interface World {
     
-    Set<GameObject> getPlayers();
+    Optional<GameObject> getPlayer(int id);
+
+    List<GameObject> getPlayers();
 
     Set<GameObject> getEntities();
 
