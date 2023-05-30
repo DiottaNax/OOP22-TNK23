61b158fe9dee51b1d9acb9ef620ddc65a114dfa4
Nax <federico.diotallevi@studio.unibo.it>
2023-05-30 11:35:36 +0200

    [DEV] updated setGameScene()

--- src/main/java/it/unibo/tnk23/view/api/GameView.java
+++ src/main/java/it/unibo/tnk23/view/api/GameView.java
@@ -1,6 +1,10 @@
 package it.unibo.tnk23.view.api;
 
+import java.util.List;
+
 import it.unibo.tnk23.core.api.GameEngine;
+import it.unibo.tnk23.game.model.api.GameMap;
+import it.unibo.tnk23.game.model.api.GameObject;
 
 public interface GameView {
 
@@ -8,7 +12,7 @@ public interface GameView {
 
     void setMenuScene();
 
-    void setGameScene();
+    void setGameScene(final List<GameObject> players, final GameMap gameMap);
 
     void setGameOverScene();
 
