f8d91963974b0a7a7e94b36b2f08c92663400343
gioele santi <gioele.santi2@studio.unibo.it>
2023-05-31 17:19:38 +0200

    [FIX] changed type of control in collisionComponent

--- src/main/java/it/unibo/tnk23/game/components/impl/CollisionComponent.java
+++ src/main/java/it/unibo/tnk23/game/components/impl/CollisionComponent.java
@@ -2,7 +2,7 @@ package it.unibo.tnk23.game.components.impl;
 
 
 import it.unibo.tnk23.common.Configuration;
-import it.unibo.tnk23.common.Point2D;
+import it.unibo.tnk23.common.Directions;
 import it.unibo.tnk23.common.shape.Rect2D;
 import it.unibo.tnk23.game.components.api.AbstractComponent;
 import it.unibo.tnk23.game.model.api.GameObject;
@@ -16,9 +16,9 @@ public class CollisionComponent extends AbstractComponent {
     public CollisionComponent(GameObject entity, World world) {
         super(entity, world);
         var type = entity.getType();
-        var width = type.getWidth() * Configuration.SCALE_FACTOR;
-        var height = type.getHeight() * Configuration.SCALE_FACTOR;
-        this.hitbox = new Rect2D(width, height,this.findCenter(width,height));
+        var width = type.getWidth() * (Configuration.SCALE_FACTOR - 0.05);
+        var height = type.getHeight() * (Configuration.SCALE_FACTOR - 0.05);
+        this.hitbox = new Rect2D(width, height,this.entity.getPosition());
     }
 
     public boolean isCollidingWith(Shape shape) {
@@ -26,22 +26,22 @@ public class CollisionComponent extends AbstractComponent {
     }
 
     public void update() {
-        var collidedList = world.getEntities().stream()
-                .filter(e -> e.getComponent(CollisionComponent.class).isPresent())
-                .filter(e -> (e.getComponent(CollisionComponent.class).get())
-                        .isCollidingWith((Shape) hitbox))
-                .toList();
+        if (!entity.getDirection().equals(Directions.NONE)) {
+            this.hitbox.setPos(this.entity.getPosition());
 
-        collidedList.forEach(e -> {
-            e.notifyComponents(() -> e, EntitiesHealthComponent.class);
-            e.notifyComponents(() -> e, BulletHealthComponent.class);
-        });
-        collidedList.forEach(e -> e.notifyComponents(() -> e, PhysicsComponent.class));
-        this.hitbox.setCenter(findCenter(entity.getType().getWidth(),entity.getType().getHeight()));
-    }
-    
-    private Point2D findCenter(double width, double height) {
-        return new Point2D(entity.getPosition().getX() + (width / 2), entity.getPosition().getY() + (height / 2));
+            var collidedList = world.getEntities().stream()
+                    .filter(e -> !e.equals(entity))
+                    .filter(e -> e.getComponent(CollisionComponent.class).isPresent())
+                    .filter(e -> e.getComponent(CollisionComponent.class).get()
+                            .isCollidingWith((Shape) hitbox))
+                    .toList();
+
+            collidedList.forEach(e -> {
+                e.notifyComponents(() -> e, EntitiesHealthComponent.class);
+                e.notifyComponents(() -> e, BulletHealthComponent.class);
+            });
+            collidedList.stream().findAny().ifPresent(e -> entity.notifyComponents(() -> e, PhysicsComponent.class));
+        }
     }
 }
 
