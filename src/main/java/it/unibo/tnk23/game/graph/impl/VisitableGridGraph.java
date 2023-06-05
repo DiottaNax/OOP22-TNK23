3bcc5a835c026365ee885bf131c697008a046099
Nax <federico.diotallevi@studio.unibo.it>
2023-06-05 20:17:43 +0200

    [FIX] during bfs nodes were not set visited and corrected removeNode() and addNode()

--- src/main/java/it/unibo/tnk23/game/graph/impl/VisitableGridGraph.java
+++ src/main/java/it/unibo/tnk23/game/graph/impl/VisitableGridGraph.java
@@ -49,18 +49,21 @@ public class VisitableGridGraph implements VisitableGraph<VisitableGridGraphNode
         return Set.copyOf(this.graph.get(node));
     }
 
-    private Stream<VisitableGridGraphNode> getAdjacentNodes(VisitableGridGraphNode node){
-        return node.getNode().getAdjacentIndexes().stream().map(VisitableGridGraphNode::new)
-                .filter(this.graphNodes::containsKey).map(this.graphNodes::get);
+    private Stream<VisitableGridGraphNode> getAdjacentNodes(final VisitableGridGraphNode node){
+        return node.getNode().getAdjacentIndexes().stream()
+                .map(VisitableGridGraphNode::new)
+                .filter(this.graphNodes::containsKey)
+                .map(this.graphNodes::get);
     }
     
     private void addAdjacencies() {
-        this.graph.entrySet().stream().parallel()
+        this.graph.entrySet().stream()
+                .parallel()
                 .forEach(e -> e.getValue().addAll(getAdjacentNodes(e.getKey()).toList()));
     }
 
     @Override
-    public List<Directions> getPathFrom(VisitableGridGraphNode node) {
+    public List<Directions> getPathFrom(final VisitableGridGraphNode node) {
         var path = new LinkedList<Directions>();
         if (this.graphNodes.containsKey(node)) {
             var current = this.graphNodes.get(node);
@@ -75,16 +78,16 @@ public class VisitableGridGraph implements VisitableGraph<VisitableGridGraphNode
         return path;
     }
 
-    private Directions detectDirection(VisitableGridGraphNode current, VisitableGridGraphNode next) {
+    private Directions detectDirection(final VisitableGridGraphNode current, final VisitableGridGraphNode next) {
         var c = current.getNode().getGraphIndex();
         var n = next.getNode().getGraphIndex();
-        return c.getX().equals(n.getX()) ? (c.getY() < n.getY() ? Directions.SOUTH : Directions.NORTH)
-                : (c.getX() < n.getX() ? Directions.EAST : Directions.WEST);
+        return c.getX().equals(n.getX()) ? c.getY() < n.getY() ? Directions.SOUTH : Directions.NORTH
+                : c.getX() < n.getX() ? Directions.EAST : Directions.WEST;
     }
 
     @Override
     public void setGoal(VisitableGridGraphNode goal) {
-        if (!this.goal.equals(goal) && this.graphNodes.containsKey(goal)) {
+        if (!this.goal.equals(goal) && this.graph.containsKey(goal)) {
             //here starts bfs
             this.graph.keySet().forEach(VisitableGridGraphNode::reset);
             var source = this.graphNodes.get(goal);
@@ -95,10 +98,12 @@ public class VisitableGridGraph implements VisitableGraph<VisitableGridGraphNode
                 var current = q.poll();
 
                 this.graph.get(current).stream()
+                        .filter(this.graph::containsKey)
                         .filter(n -> !n.isVisited())
                         .forEach(n -> {
                             n.setDistance(current.getDistance() + 1);
                             n.setParent(current);
+                            n.setVisited();
                             n.setDirectionToParent(detectDirection(n, current));
                             q.add(n);
                         });
@@ -121,6 +126,7 @@ public class VisitableGridGraph implements VisitableGraph<VisitableGridGraphNode
     public void removeNode(VisitableGridGraphNode node) {
         this.graphNodes.remove(node);
         this.graph.remove(node);
+        this.getAdjacentNodes(node).forEach(n -> this.graph.get(n).remove(node));
     }
 
     @Override
@@ -136,7 +142,12 @@ public class VisitableGridGraph implements VisitableGraph<VisitableGridGraphNode
     public VisitableGridGraphNode addNode(VisitableGridGraphNode node) {
         if (!this.graphNodes.containsKey(node)) {
             this.graphNodes.put(node, node);
-            this.graph.put(node, this.getAdjacentNodes(node).collect(Collectors.toCollection(HashSet::new)));
+            var adjacentNodes = this.getAdjacentNodes(node).toList();
+            this.graph.put(node, adjacentNodes.stream().collect(Collectors.toCollection(HashSet::new)));
+            adjacentNodes.forEach(n -> {
+                this.graph.get(n).remove(node);
+                this.graph.get(n).add(node);
+            });
             return node;
         }
 
