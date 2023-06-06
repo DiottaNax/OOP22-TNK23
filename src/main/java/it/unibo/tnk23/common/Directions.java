f2efb4480ff168f7145ff34359a080bb1466ee7b
Nax <federico.diotallevi@studio.unibo.it>
2023-06-06 22:08:58 +0200

    [DOC] write javadoc

--- src/main/java/it/unibo/tnk23/common/Directions.java
+++ src/main/java/it/unibo/tnk23/common/Directions.java
@@ -1,5 +1,9 @@
 package it.unibo.tnk23.common;
 
+/**
+ * The {@code Directions} enum represents different directions in a two-dimensional space.
+ * Each direction is associated with a velocity vector represented by the {@link Vector2D} class.
+ */
 public enum Directions {
     NORTH(0, -1),
     SOUTH(0, 1),
@@ -9,14 +13,32 @@ public enum Directions {
 
     private Vector2D vel;
 
+    /**
+     * Constructs a direction with the specified x and y components of the velocity vector.
+     *
+     * @param x The x component of the velocity vector.
+     * @param y The y component of the velocity vector.
+     */
     Directions(final int x, final int y) {
         vel = new Vector2D(x, y);
     }
 
+    /**
+     * Retrieves the velocity vector associated with the direction.
+     *
+     * @return The velocity vector of the direction.
+     */
     public Vector2D getVel() {
         return this.vel;
     }
 
+    /**
+     * Converts a {@link Vector2D} object into the corresponding {@code Directions} enum value.
+     * If the provided vector does not match any predefined direction, {@code NONE} is returned.
+     *
+     * @param v The vector to convert into a direction.
+     * @return The {@code Directions} enum value corresponding to the provided vector.
+     */
     public static Directions fromVector(Vector2D v) {
         for (var d : values()) {
             if (d.vel.equals(v)) {
@@ -26,20 +48,33 @@ public enum Directions {
         return NONE;
     }
 
+    /**
+     * Returns the opposite direction of the current direction.
+     *
+     * @return The opposite direction of the current direction.
+     */
     public Directions flipped() {
         return fromVector(this.vel.mul(-1));
     }
 
+    /**
+     * Converts an angle (in degrees) into the corresponding {@code Directions} enum value.
+     * It maps specific angles to the predefined directions (0 degrees - {@code NORTH},
+     * 90 degrees - {@code EAST}, -90 degrees - {@code WEST}, 180 degrees - {@code SOUTH}).
+     *
+     * @param angle The angle to convert into a direction.
+     * @return The {@code Directions} enum value corresponding to the provided angle.
+     */
     public static Directions fromAngle(int angle) {
         var dir = NONE;
         switch (angle) {
             case 0:
                 dir = NORTH;
                 break;
-            case 90:
+            case 90: //JavaFX calculates the rotation clockwise
                 dir = EAST;
                 break;
-            case -90:
+            case -90: //JavaFX calculates the rotation clockwise
                 dir = WEST;
                 break;
             case 180:
