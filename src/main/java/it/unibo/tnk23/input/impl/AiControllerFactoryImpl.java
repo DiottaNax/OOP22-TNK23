552dc6d4a52931bb9557254b5d5a94621e689c73
Nax <federico.diotallevi@studio.unibo.it>
2023-06-07 01:08:05 +0200

    [DOC] added javadoc

--- src/main/java/it/unibo/tnk23/input/impl/AiControllerFactoryImpl.java
+++ src/main/java/it/unibo/tnk23/input/impl/AiControllerFactoryImpl.java
@@ -3,11 +3,8 @@ package it.unibo.tnk23.input.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.stream.Stream;
 
-import it.unibo.tnk23.common.Configuration;
 import it.unibo.tnk23.common.Directions;
 import it.unibo.tnk23.game.graph.impl.GameGraph;
 import it.unibo.tnk23.game.model.api.GameObject;
@@ -15,35 +12,64 @@ import it.unibo.tnk23.game.model.api.World;
 import it.unibo.tnk23.input.api.AiControllerFactory;
 import it.unibo.tnk23.input.api.InputController;
 
+/**
+ * The {@code AiControllerFactoryImpl} class implements the {@link AiControllerFactory} interface
+ * and provides methods to create AI controllers for different scenarios.
+ * 
+ * @author Federico Diotallevi
+ */
 public class AiControllerFactoryImpl implements AiControllerFactory {
     private final GameGraph graph;
     private final World world;
 
+    /**
+     * Constructs an {@code AiControllerFactoryImpl} with the specified game graph and world.
+     *
+     * @param graph The game graph.
+     * @param world The game world.
+     */
     public AiControllerFactoryImpl(final GameGraph graph, final World world) {
         this.graph = graph;
         this.graph.setWorld(world);
         this.world = world;
     }
 
+    /**
+     * Generates a pseudo-random direction different from the given direction.
+     * It is less likely that the next direction is equal to the previous or to {@code NONE}.
+     *
+     * @param dir The current direction.
+     * @return A pseudo-random direction.
+     */
     private Directions getPseudoRandomDir(Directions dir) {
         var possibilities = new ArrayList<>(
                 List.of(Directions.NONE, Directions.SOUTH, Directions.WEST, Directions.EAST));
         possibilities.addAll(possibilities);
         possibilities.remove(dir);
+        possibilities.remove(Directions.NONE);
         return possibilities.get(new Random().nextInt(possibilities.size()));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public InputController getRandomAi() {
         var iterator = Stream.iterate(Directions.SOUTH, this::getPseudoRandomDir).iterator();
         return () -> iterator.next();       
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public InputController getFollowTowerAi(GameObject entity) {
         return new FollowTargetAi(graph, entity, this.world.getTower());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public InputController getFollowMovingTargetAi(GameObject entity, GameObject target) {
         return new FollowTargetAi(graph, entity, target);
--- src/main/java/it/unibo/tnk23/input/impl/FollowTargetAi.java
+++ src/main/java/it/unibo/tnk23/input/impl/FollowTargetAi.java
@@ -1,7 +1,6 @@
 package it.unibo.tnk23.input.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Timer;
@@ -12,10 +11,17 @@ import it.unibo.tnk23.game.graph.impl.GameGraph;
 import it.unibo.tnk23.game.model.api.GameObject;
 import it.unibo.tnk23.input.api.InputController;
 
+/**
+ * The {@code FollowTargetAi} class implements the {@link InputController} interface
+ * to provide an AI input controller that follows a target game object.
+ * It uses a {@link GameGraph} to calculate the path to the target and provides the next direction to move.
+ * 
+ * @author Federico Diotallevi
+ */
 public class FollowTargetAi implements InputController {
 
     private final static long UPDATE_PERIOD = 2000;
-    private List<Directions> path = Collections.synchronizedList(new ArrayList<>());
+    private List<Directions> path = new ArrayList<>();
     private Iterator<Directions> iterator;
     private final GameGraph graph;
     private final GameObject entity;
@@ -23,6 +29,13 @@ public class FollowTargetAi implements InputController {
     private boolean timeToUpdate = true;
     private final Timer timer;
 
+    /**
+     * Constructs a new instance of {@code FollowTargetAi} with the specified game graph, entity, and target.
+     *
+     * @param graph  The game graph used for pathfinding.
+     * @param entity The entity controlled by the AI.
+     * @param target The target game object to follow.
+     */
     public FollowTargetAi(GameGraph graph, GameObject entity, GameObject target) {
         this.graph = graph;
         this.entity = entity;
@@ -31,6 +44,9 @@ public class FollowTargetAi implements InputController {
         this.startUpdate();
     }
 
+    /**
+     * Starts the periodic update task to recalculate the path to the target.
+     */
     public void startUpdate() {
         timer.schedule(new TimerTask() {
             @Override
@@ -40,6 +56,9 @@ public class FollowTargetAi implements InputController {
         }, 0, UPDATE_PERIOD);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Directions getDirection() {
         if (timeToUpdate) {
@@ -47,6 +66,9 @@ public class FollowTargetAi implements InputController {
             path.clear();
             this.path.addAll(this.graph.getPathFrom(entity.getPosition()));
             if (this.path.stream().allMatch(d -> d.equals(Directions.NONE))) {
+                /*
+                 * The enemy goes backwards if, due to some errors, it ends in a position not present in the game graph.
+                 */
                 for (int i = 0; i < 3; i++) {
                     this.path.add(Directions.fromAngle((int) entity.getRotation()).flipped());
                 }
