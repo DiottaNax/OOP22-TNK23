3911e4d9fe108a55362568616db8cf7c82b8f28b
Nax <federico.diotallevi@studio.unibo.it>
2023-06-06 20:00:46 +0200

    renamed FollowMovingTargetAi to FollowTargetAi

--- src/main/java/it/unibo/tnk23/input/impl/FollowTargetAi.java
+++ src/main/java/it/unibo/tnk23/input/impl/FollowTargetAi.java
@@ -2,20 +2,17 @@ package it.unibo.tnk23.input.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.stream.Stream;
 
 import it.unibo.tnk23.common.Directions;
-import it.unibo.tnk23.game.graph.api.VisitableGraph;
 import it.unibo.tnk23.game.graph.impl.GameGraph;
 import it.unibo.tnk23.game.model.api.GameObject;
 import it.unibo.tnk23.input.api.InputController;
 
-public class FollowMovingTargetAi implements InputController {
+public class FollowTargetAi implements InputController {
 
     private final static long UPDATE_PERIOD = 2000;
     private List<Directions> path = Collections.synchronizedList(new ArrayList<>());
@@ -26,7 +23,7 @@ public class FollowMovingTargetAi implements InputController {
     private boolean timeToUpdate = true;
     private final Timer timer;
 
-    public FollowMovingTargetAi(GameGraph graph, GameObject entity, GameObject target) {
+    public FollowTargetAi(GameGraph graph, GameObject entity, GameObject target) {
         this.graph = graph;
         this.entity = entity;
         this.target = target;
@@ -46,20 +43,17 @@ public class FollowMovingTargetAi implements InputController {
     @Override
     public Directions getDirection() {
         if (timeToUpdate) {
-            //System.out.println("TARGET: " + target.getPosition());
             this.graph.setGoal(target.getPosition());
             path.clear();
-            //System.out.println("FOLLOWER: " + entity.getPosition());
             this.path.addAll(this.graph.getPathFrom(entity.getPosition()));
-            if (this.path.isEmpty()) {
+            if (this.path.stream().allMatch(d -> d.equals(Directions.NONE))) {
                 for (int i = 0; i < 3; i++) {
-                    this.path.add(this.entity.getDirection().flipped());
+                    this.path.add(Directions.fromAngle((int) entity.getRotation()).flipped());
                 }
                 path.add(Directions.NONE);
             }
             iterator = this.path.iterator();
             timeToUpdate = false;
-            System.out.println(path);
         }
 
         return !iterator.hasNext() ? Directions.NONE : iterator.next();
