746d59ce73e7e2567d5dbeffacd25661c96d9e4b
Nax <federico.diotallevi@studio.unibo.it>
2023-06-06 21:40:07 +0200

    [DOC] Added javadoc

--- src/main/java/it/unibo/tnk23/core/api/GameLoop.java
+++ src/main/java/it/unibo/tnk23/core/api/GameLoop.java
@@ -1,16 +1,37 @@
 package it.unibo.tnk23.core.api;
 
-import it.unibo.tnk23.game.events.api.WorldEvent;
 import it.unibo.tnk23.game.events.api.WorldEventListener;
 
+/**
+ * The {@code GameLoop} interface represents the main game loop for TNK23.
+ * It defines the methods for processing input, updating the game state, rendering the game, and obtaining the game engine instance.
+ * The interface extends the {@link WorldEventListener} interface, allowing it to receive and handle world events.
+ */
 public interface GameLoop extends WorldEventListener{
 
+    /**
+     * Processes events for the game.
+     * This method should handle all the events passed to the game loop.
+     */
     void processInput();
 
+    /**
+     * Updates the game state.
+     * This method should update the game state based on user input, AI behavior, physics, etc.
+     */
     void update();
 
+    /**
+     * Renders the game.
+     * This method should render the game view based on the current game state.
+     */
     void render();
 
+    /**
+     * Retrieves the game engine instance.
+     *
+     * @return the game engine instance associated with the game loop.
+     */
     GameEngine getGameEngine();
 
 }
--- src/main/java/it/unibo/tnk23/core/impl/GameLoopImpl.java
+++ src/main/java/it/unibo/tnk23/core/impl/GameLoopImpl.java
@@ -11,40 +11,67 @@ import it.unibo.tnk23.game.events.api.WorldEventHandler;
 import it.unibo.tnk23.game.events.impl.WorldEventHandlerImpl;
 import it.unibo.tnk23.game.model.api.World;
 
+/**
+ * The {@code GameLoopImpl} class implements the {@link GameLoop} interface to provide the main game loop functionality for TNK23.
+ * It updates the game state, renders the game view, and handles world events.
+ * 
+ * @author Federico Diotallevi
+ */
 public class GameLoopImpl implements GameLoop {
     private final GameEngine engine;
     private final World wrld;
     private final WorldEventHandler eventHandler;
     private List<WorldEvent> eventQueue = Collections.synchronizedList(new LinkedList<>());
     
+    /**
+     * Constructs a {@code GameLoopImpl} instance with the given game engine.
+     * Initializes the world, event handler, and event queue.
+     *
+     * @param engine the game engine associated with the game loop.
+     */
     public GameLoopImpl(final GameEngine engine) {
         this.engine = engine;
         this.wrld = engine.getWorld();
         this.eventHandler = new WorldEventHandlerImpl(wrld);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public GameEngine getGameEngine() {
         return this.engine;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void processInput() {
         this.eventQueue.forEach(eventHandler::handle);
         this.eventQueue.clear();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void update() {
         this.engine.getGameState().update();
         this.wrld.update();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void render() {
         this.engine.getGameView().renderView();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void notifyEvent(WorldEvent e) {
         this.eventQueue.add(e);
