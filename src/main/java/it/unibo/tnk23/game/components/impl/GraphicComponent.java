260b1ecb976a96f22412cdd5d7bac723bd6f8166
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-07 20:24:47 +0200

    [DEV] made javadoc in Components and wordImpl

--- src/main/java/it/unibo/tnk23/game/components/impl/GraphicComponent.java
+++ src/main/java/it/unibo/tnk23/game/components/impl/GraphicComponent.java
@@ -3,25 +3,47 @@ package it.unibo.tnk23.game.components.impl;
 import it.unibo.tnk23.game.components.api.Component;
 import it.unibo.tnk23.game.model.api.GameObject;
 
+/**
+ * Represents a graphic component for game entities.
+ * Handles the rendering and sprite management for entities.
+ */
 public class GraphicComponent implements Component {
 
     private String spritename;
 
+    /**
+     * Constructs a GraphicComponent for the given entity and sprite name.
+     *
+     * @param entity the entity to which this component belongs
+     * @param spriteName the name of the sprite associated with the entity
+     */
     public GraphicComponent(GameObject entity, String spriteName) {
         this.spritename = spriteName;
     }
 
+    /**
+     {@inheritDoc}
+     */
     @Override
     public void update() {
     }
 
+    /**
+     * Gets the name of the sprite associated with the entity.
+     *
+     * @return the sprite name
+     */
     public String getSpriteName() {
         return this.spritename;
     }
 
-    public void setSpriteName( String spriteName) {
+    /**
+     * Sets the name of the sprite associated with the entity.
+     *
+     * @param spriteName the sprite name to set
+     */
+    public void setSpriteName(String spriteName) {
         this.spritename = spriteName;
     }
 
 }
-
