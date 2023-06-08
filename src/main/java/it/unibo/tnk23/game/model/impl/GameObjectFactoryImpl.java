19232396824f3fd8fb1124000883d124d9a4aba5
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-08 11:47:21 +0200

    [FIX] fixed name of component

--- src/main/java/it/unibo/tnk23/game/model/impl/GameObjectFactoryImpl.java
+++ src/main/java/it/unibo/tnk23/game/model/impl/GameObjectFactoryImpl.java
@@ -1,7 +1,6 @@
 package it.unibo.tnk23.game.model.impl;
 
 import it.unibo.tnk23.common.Point2D;
-import it.unibo.tnk23.game.components.impl.BulletCollisionComponent;
 import it.unibo.tnk23.game.components.impl.BulletComponent;
 import it.unibo.tnk23.game.components.impl.BulletHealthComponent;
 import it.unibo.tnk23.game.components.impl.CollisionComponent;
@@ -67,7 +66,7 @@ public class GameObjectFactoryImpl implements GameObjectFactory {
         var bullet = new GameObjectImpl(TypeObjectFactory.getBulletType(), pos);
         bullet.addComponent(new BulletHealthComponent(bullet, world));
         bullet.addComponent(new PhysicsComponent(bullet, world));
-        bullet.addComponent(new BulletCollisionComponent(bullet, world));
+        bullet.addComponent(new CollisionComponent(bullet, world));
         bullet.addComponent(new BulletComponent(bullet, world));
         bullet.addComponent(new GraphicComponent("bullet"));
         return bullet;
