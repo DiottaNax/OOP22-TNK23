92594f0b5b2f90b6a86c88425feb0b5ed0ba54f1
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-08 10:59:32 +0200

    [STYLE] corretted checkstyle errors

--- src/main/java/it/unibo/tnk23/game/components/impl/PhysicsComponent.java
+++ src/main/java/it/unibo/tnk23/game/components/impl/PhysicsComponent.java
@@ -18,6 +18,7 @@ import it.unibo.tnk23.game.model.impl.TypeObjectFactory;
 public class PhysicsComponent extends AbstractComponent implements NotifiableComponent {
 
     private double speed;
+    private int angle = 180;
 
     /**
      * Constructs a PhysicsComponent for the given entity and world.
@@ -25,7 +26,7 @@ public class PhysicsComponent extends AbstractComponent implements NotifiableCom
      * @param entity the entity to which this component belongs
      * @param world the game world
      */
-    public PhysicsComponent(GameObject entity, World world) {
+    public PhysicsComponent(final GameObject entity, final World world) {
         super(entity, world);
         var type = entity.getType();
         this.speed = ((double) Configuration.TILE_SIZE / Configuration.FPS) * type.getSpeed();
@@ -39,17 +40,21 @@ public class PhysicsComponent extends AbstractComponent implements NotifiableCom
         var nextPos = this.getEntity().getPosition().sum(this.getEntity().getDirection().getVel().mul(this.speed));
 
         if (nextPos.getX() >= Configuration.DISPLACEMENT
-                && nextPos.getX() <= Configuration.GAME_SCENE_DIMENSION - this.getEntity().getType().getWidth() - Configuration.DISPLACEMENT
+                && nextPos.getX() <= Configuration.GAME_SCENE_DIMENSION
+                - this.getEntity().getType().getWidth() - Configuration.DISPLACEMENT
                 && nextPos.getY() >= Configuration.DISPLACEMENT
-                && nextPos.getY() <= Configuration.GAME_SCENE_DIMENSION - this.getEntity().getType().getHeight() - Configuration.DISPLACEMENT) {
+                && nextPos.getY() <= Configuration.GAME_SCENE_DIMENSION
+                - this.getEntity().getType().getHeight() - Configuration.DISPLACEMENT) {
 
             this.getEntity().setPosition(nextPos);
         } else if (TypeObjectFactory.isBullet(this.getEntity().getType())) {
-            this.getWorld().notifyEvent(new WorldEventImpl(this.getEntity().getPosition(), this.getEntity(), WorldEventType.DEATH_EVENT));
+            this.getWorld().notifyEvent(
+                    new WorldEventImpl(this.getEntity().getPosition(), this.getEntity(), WorldEventType.DEATH_EVENT));
         }
-        var rotation = this.getEntity().getDirection().getVel().getX() * (90) + this.getEntity().getDirection().getVel().getY() * 180;
+        var rotation = this.getEntity().getDirection().getVel().getX() * (angle / 2)
+                + this.getEntity().getDirection().getVel().getY() * angle;
         if (rotation != 0) {
-            this.getEntity().setRotation(rotation != -180 ? rotation : 0);
+            this.getEntity().setRotation(rotation != (-angle) ? rotation : 0);
         }
     }
 
@@ -75,10 +80,11 @@ public class PhysicsComponent extends AbstractComponent implements NotifiableCom
      {@inheritDoc}
      */
     @Override
-    public <X> void receive(Message<X> x) {
+    public <X> void receive(final Message<X> x) {
         if (x.getMessage() instanceof GameObject) {
             if (!this.getEntity().getDirection().equals(Directions.NONE)) {
-                this.getEntity().setPosition(this.getEntity().getPosition().sum(this.getEntity().getDirection().getVel().mul(-this.speed)));
+                this.getEntity().setPosition(
+                        this.getEntity().getPosition().sum(this.getEntity().getDirection().getVel().mul(-this.speed)));
             }
         }
     }
