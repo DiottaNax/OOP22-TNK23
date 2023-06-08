2dc440513ce1465ef5b675d1fdfb43b60d2ef133
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-08 10:57:59 +0200

    [DEV] added missing javadoc

--- src/main/java/it/unibo/tnk23/game/model/api/GameMap.java
+++ src/main/java/it/unibo/tnk23/game/model/api/GameMap.java
@@ -3,7 +3,11 @@ package it.unibo.tnk23.game.model.api;
 import java.util.Set;
 import it.unibo.tnk23.common.Point2D;
 
+/**
+ * The GameMap interface represents a game map that provides information about the game's walls and destroyable walls.
+ */
 public interface GameMap {
+
     /**
      * Retrieves the set of destroyable walls in the game map.
      *
