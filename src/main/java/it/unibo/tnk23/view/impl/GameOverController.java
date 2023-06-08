4d161fddef8e599c2a055939b01967bdeae5bd84
Javid Ameri <javid.ameri@studio.unibo.it>
2023-06-08 03:20:46 +0200

    added javadocs

--- src/main/java/it/unibo/tnk23/view/impl/GameOverController.java
+++ src/main/java/it/unibo/tnk23/view/impl/GameOverController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 
+/**
+ * The GameOverController class is responsible for handling the game over screen and related actions.
+ * It provides functionality to restart the game or exit the application.
+ */
 public class GameOverController {
 
     @FXML
@@ -15,15 +19,29 @@ public class GameOverController {
     private FxGameView view;
 
 
+    /**
+     * Constructs a new GameOverController object with the specified FxGameView.
+     * 
+     * @param view The FxGameView object to associate with the controller.
+     */
     public GameOverController(FxGameView view) {
         this.view = view;
     }
 
+    /**
+     * Handles the action event when the restart button is clicked.
+     * Sets a new world-object and then switches to the menu scene to restart the game.
+     */
     @FXML
     private void restartGame() {
+        this.view.setDefaultWorld();
         this.view.setMenuScene();
     }
     
+    /**
+     * Handles the action event when the exit button is clicked.
+     * Exits the application by terminating the JVM.
+     */
     @FXML
     private void exit() {     
         System.exit(0);
