2f8168bea6a185af060b37f0d04d21734b94069d
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-08 11:33:23 +0200

    [DEV] eliminated BulletCollisionComponent, moved the control for the bullet in CollisionComponent

--- src/main/java/it/unibo/tnk23/game/components/impl/CollisionComponent.java
+++ src/main/java/it/unibo/tnk23/game/components/impl/CollisionComponent.java
@@ -1,11 +1,15 @@
 package it.unibo.tnk23.game.components.impl;
 
+
 import it.unibo.tnk23.common.Configuration;
 import it.unibo.tnk23.common.Directions;
 import it.unibo.tnk23.common.shape.Rect2D;
 import it.unibo.tnk23.game.components.api.AbstractComponent;
+import it.unibo.tnk23.game.events.api.WorldEventType;
+import it.unibo.tnk23.game.events.impl.WorldEventImpl;
 import it.unibo.tnk23.game.model.api.GameObject;
 import it.unibo.tnk23.game.model.api.World;
+import it.unibo.tnk23.game.model.impl.TypeObjectFactory;
 import it.unibo.tnk23.common.shape.Shape;
 
 /**
@@ -15,6 +19,10 @@ import it.unibo.tnk23.common.shape.Shape;
 public class CollisionComponent extends AbstractComponent {
 
     private Rect2D hitbox;
+    /**
+    * Sensibility constant used for approximation of the hitbox.
+    */
+    private static final double SENSIBILITY = 0.05;
 
     /**
      * Constructs a CollisionComponent for the given entity and world.
@@ -22,11 +30,11 @@ public class CollisionComponent extends AbstractComponent {
      * @param entity the entity to which this component belongs
      * @param world the game world
      */
-    public CollisionComponent(GameObject entity, World world) {
+    public CollisionComponent(final GameObject entity, final World world) {
         super(entity, world);
         var type = entity.getType();
-        var width = type.getWidth() * (Configuration.SCALE_FACTOR - 0.05);
-        var height = type.getHeight() * (Configuration.SCALE_FACTOR - 0.05);
+        var width = type.getWidth() * (Configuration.SCALE_FACTOR - SENSIBILITY);
+        var height = type.getHeight() * (Configuration.SCALE_FACTOR - SENSIBILITY);
         this.hitbox = new Rect2D(width, height, this.getEntity().getPosition());
     }
 
@@ -36,7 +44,7 @@ public class CollisionComponent extends AbstractComponent {
      * @param shape the shape to check for collision
      * @return true if the entity is colliding with the shape, false otherwise
      */
-    public boolean isCollidingWith(Shape shape) {
+    public boolean isCollidingWith(final Shape shape) {
         return this.hitbox.isColliding(shape);
     }
 
@@ -54,6 +62,10 @@ public class CollisionComponent extends AbstractComponent {
                     .filter(e -> e.getComponent(CollisionComponent.class).get()
                             .isCollidingWith((Shape) hitbox))
                     .toList();
+            
+             if (!collidedList.isEmpty() && TypeObjectFactory.isBullet(this.getEntity().getType())) {
+                this.getWorld().notifyEvent(new WorldEventImpl(this.getEntity().getPosition(), this.getEntity(), WorldEventType.DEATH_EVENT));
+            }
 
             collidedList.forEach(e -> {
                 e.notifyComponents(() -> this.getEntity(), EntitiesHealthComponent.class);
