c0ecc533126f57423c3074cdabef70b960ec86a1
gioele santi <gioele.santi2@studio.unibo.it>
2023-06-09 12:37:52 +0200

    [DEV] made javadoc

--- src/main/java/it/unibo/tnk23/view/api/RenderingEngine.java
+++ src/main/java/it/unibo/tnk23/view/api/RenderingEngine.java
@@ -1,5 +1,10 @@
 package it.unibo.tnk23.view.api;
 
+/**
+ * The RenderingEngine interface provides a contract for rendering the game.
+ *
+ * @param <X> the type of the game pane used for rendering
+ */
 public interface RenderingEngine<X> {
 
     /**
